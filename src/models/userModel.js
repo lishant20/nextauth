1de768b44e7f21905bf2b8112ea29100c032beac
Lishant <lishantkarki@gmail.com>
2025-11-29 23:09:19 +0545

    Folder structure and usermodel

---
+++ src/models/userModel.js
@@ -0,0 +1,35 @@
+import { verify } from "crypto";
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unqiue: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    rquired: [true, 'Please provide email']
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"]
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: String
+})
+
+const user = mongoose.model('user',userSchema)
+
+export default user;
