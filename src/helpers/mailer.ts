dad6b08918e75f425e577094116b5e71c1957575
Lishant <lishantkarki@gmail.com>
2025-11-30 13:22:23 +0545

    signup and mailer setup

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,16 +1,25 @@
+import User from '@/models/userModel';
+import bcrypt from 'bcryptjs';
 import nodemailer from 'nodemailer';
 
 export const sendEmail = async({email, emailType, userId}:any) => {
   try {
 
-    //Todo: Configure mail for usage
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 465,
-      secure: true,
+    const hashedToken = await bcrypt.hash(userId.toString(), 10)
+
+    if(emailType === "VERIFY"){
+      await User.findByIdAndUpdate(userId, {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000});
+    }else if(emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000});
+    }
+
+
+    const transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
       auth: {
-        user: "maddison53@ethereal.email",
-        pass: "jn7jnAPss4f63QBp6D",
+        user: "771eee5c2fedcb", // wrong
+        pass: "c660b18b9fcc3b" // wrong
       }
     });
 
@@ -18,10 +27,12 @@ export const sendEmail = async({email, emailType, userId}:any) => {
     from: 'lishantkarki@gmail.com',
     to: email,
     subject: emailType === 'VERIFY'?"Verify your email": "Reset your password",
-    html: "<b>Hello world?</b>", // HTML body
+    html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to $
+    {emailType === "VERIFY" ? "verify your email" : "reset your password"}</p>" or copy and paste 
+    the link below in your browser.<br>${process.env.DOMAIN}/verifyemail?token=${hashedToken}</p>`
   }
 
-  const mailResponse = await transporter.sendMail(mailOptions)
+  const mailResponse = await transport.sendMail(mailOptions)
   return mailResponse;
   } catch (error:any) {
     throw new Error(error.message)
